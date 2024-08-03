fe9804285fabf5c82e284de46dda2413aaf0ab91
Camila Yukari Yatabe <camila.yatabe@gmail.com>
2024-08-02 21:03:54 -0300

    fix: footer to stay at the bottom

--- frontend/src/app/components/footer.tsx
+++ frontend/src/app/components/footer.tsx
@@ -2,9 +2,7 @@
 export function Footer(){
 
     return (
-    
-        <footer className="p-6 bg-white text-center 
-              text-pink-900 text-sm fixed bottom-0 left-0 w-full">
+      <footer className="mt-4 p-6 bg-white text-center text-pink-900 text-sm" >
         Todos os direitos reservados &copy; 2024
       </footer>
         
