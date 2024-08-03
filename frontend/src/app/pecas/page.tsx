7754594d9020e01d5eccac855f30e64fc5aca2f3
Camila Yukari Yatabe <camila.yatabe@gmail.com>
2024-08-02 21:22:11 -0300

    feat: add cards to peças

--- frontend/src/app/pecas/page.tsx
+++ frontend/src/app/pecas/page.tsx
@@ -4,7 +4,9 @@ import { Aside } from "../components/aside";
 import Title from "../components/title";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import FormsMaquinas from "../components/form_maquina";
+import FormsPecas from "../components/form_pecas";
+import { Card } from "../components/card";
+
 
 export default function Home() {
     return (
@@ -13,11 +15,33 @@ export default function Home() {
             <div className="flex flex-1 overflow-hidden">
                 <Aside />
                 <main className="flex-1 flex flex-col overflow-hidden">
-                    <Title text="Gerenciamento de máquinas" />
+                    <Title text="Controle de estoque de peças" />
                     <div className="flex-1 overflow-y-auto p-6">
+                    <div className="grid grid-cols-4 gap-4 mb-6">
+              <Card
+                color="bg-pink-800 p-6 flex gap-2 rounded-xl"
+                quantity="100"
+                text="Ambientes"
+              />
+              <Card
+                color="bg-pink-800 p-6 flex gap-2 rounded-xl"
+                quantity="140"
+                text="Equipamentos"
+              />
+              <Card
+                color="bg-pink-800 p-6 flex gap-2 rounded-xl"
+                quantity="210"
+                text="O.S. Abertas"
+              />
+              <Card
+                color="bg-pink-800 p-6 flex gap-2 rounded-xl"
+                quantity="120"
+                text="O.S. Concluídas"
+              />
+            </div>
 
                         <div className="container mx-auto">
-                            <FormsMaquinas />
+                            <FormsPecas />
 
                             <div className="mx-auto max-w-md"></div>
                         </div>
