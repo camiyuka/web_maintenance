53029cc5697174003476aa27e3e539ed236b96a2
camilayatabe <camila.yatabe@gmail.com>
2024-08-23 22:42:03 -0300

    fix: remove machine details from page

--- frontend/src/app/maquinas/page.tsx
+++ frontend/src/app/maquinas/page.tsx
@@ -6,40 +6,8 @@ import Title from "../components/title";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FormsMaquinas from "../components/form_maquina";
-import MachineDetails from "../components/machine_details"
 
 export default function Home() {
-    
-const maquina = {
-    name: "Máquina 1",
-    type: "Tipo A",
-    model: "Modelo X",
-    manufactureDate: "2020-01-01",
-    serialNumber: "12345",
-    location: "Fábrica 1",
-    maintenanceHistory: [
-      {
-        date: "2021-06-01",
-        description: "Troca de óleo",
-        responsible: "José Silva",
-        status: "Concluída",
-        comments: "Manutenção realizada sem problemas.",
-        partsUsed: [
-          { name: "Óleo Lubrificante", quantity: "2 Litros", supplier: "Fornecedor A" },
-        ],
-      },
-      {
-        date: "2022-03-15",
-        description: "Substituição de filtro",
-        responsible: "Maria Souza",
-        status: "Pendente",
-        comments: "Aguardando peças.",
-        partsUsed: [
-          { name: "Filtro de Ar", quantity: "1 Unidade", supplier: "Fornecedor B" },
-        ],
-      },
-    ],
-  };
 
     return (
 
@@ -54,8 +22,6 @@ const maquina = {
                             <FormsMaquinas />
 
                             <div className="mx-auto max-w-md"></div>
-                            <h1 className="text-2xl font-bold text-center p-5"> Visualização das Máquinas </h1>
-                            <MachineDetails machine={maquina} />
                         </div>
                     </div>
                 </main>
